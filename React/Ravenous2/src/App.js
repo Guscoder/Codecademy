c9a5245da299a3f6844393b34b3b52d1d326d3b7
Justin Gustafson <jtwgus@gmail.com>
2019-03-08 09:46:28 -0500

    Ravenous project

---
+++ React/Ravenous2/src/App.js
@@ -0,0 +1,46 @@
+/* 
+App does connect to Business, but not directly.
+• App imports BusinessList.
+• BusinessList imports Business.
+
+When App calls BusinessList, App passes props to BusinessList.  In turn, BusinessList is able to pass those same props to Business.
+ */
+
+
+import React from 'react';
+import './App.css';
+import BusinessList from './components/BusinessList/BusinessList.js';
+import SearchBar from './components/SearchBar/SearchBar.js';
+import Yelp from './util/Yelp.js';
+
+
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      businesses: []
+    };
+    this.searchYelp = this.searchYelp.bind(this);
+  }
+
+  searchYelp(term, location, sortBy) {
+    Yelp.search(term, location, sortBy).then(
+      business => {
+        this.setState({businesses: business});
+      }
+    );
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>ravenous</h1>
+        <SearchBar searchYelp={this.searchYelp}/>
+        <BusinessList businesses={this.state.businesses}/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ React/Ravenous2/src/util/Yelp.js
@@ -0,0 +1,38 @@
+const apiKey = 'AttpC1YMw3z0qNHfjjEtgI2D0fYEvDmDDFOQA_L7BBP1aqE_PPkZ-JPk0yTTjCaHus9RVPB92bK6orTltg7SOgLx9-hjOXLPsVc-mxlg8UMpcuy6NEmheZ-rNph1XHYx';
+
+// To authenticate API calls with the API Key, set the Authorization HTTP header value as Bearer API_KEY.
+
+  const Yelp = {
+    search(term, location, sortBy){
+        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
+            headers: {
+                Authorization: `Bearer ${apiKey}`
+            }
+        }).then(
+            (response) => {
+                return response.json();
+            }
+        ).then(
+            (jsonResponse) => {
+                if(jsonResponse.businesses) {
+                    return jsonResponse.businesses.map((business) => {
+                        return {
+                            id: business.id,
+                            imageSrc: business.image_url,
+                            name: business.name,
+                            address: business.location.address1,
+                            city: business.location.city,
+                            state: business.location.state,
+                            zipCode: business.location.zip_code,
+                            category: business.categories[0].title,
+                            rating: business.rating,
+                            reviewCount:business.review_count
+                        }
+                    })
+                }
+            }
+        );
+    },
+}
+
+export default Yelp;
